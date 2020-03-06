ac7ef811ebf7a7cae2a24a3768290c8e52d29297
rjr2274 <rjr2274@me.com>
2020-03-05 21:45:06 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,40 +9,6 @@ class Student < InteractiveRecord
         attr_accessor col_name.to_sym
       end
 
-      def table_name_for_insert
-        self.class.table_name
-      end
-
-      def col_names_for_insert
-        self.class.column_names.delete_if {|col| col == 'id'}.join(", ")
-      end
-
-      def values_for_insert
-        values = []
-        self.class.column_names.each do |col_name|
-          values << "'#{send(col_name)}'" unless send(col_name).nil?
-        end
-        values.join(", ")
-      end
-
-      def save
-        sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-        DB[:conn].execute(sql)
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-      end
-
-      def self.find_by_name(name)
-        sql = "SELECT * FROM #{table_name} WHERE name = '#{name}'"
-        DB[:conn].execute(sql)
-      end
-
-      def self.find_by(data)
-        binding.pry
-         
-        sql = 'SELECT * FROM #{table_name} WHERE "#{data.keys[0].to_s}" = "#{data.values[0]}"'
-        
-        DB[:conn].execute(sql)
-
-      end
+    
 
 end
